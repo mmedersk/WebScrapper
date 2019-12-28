9b3e477b0b9dfeab9cf9c8bccf7294d48167c271
Mikolaj Mederski <RWXG48@motorolasolutions.com>
2019-12-28 21:13:46 +0100

    csv and txt export, basic excetion handling

---
+++ WebScrapper/CleaningHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace WebScrapper
+{
+    public class CleaningHandler
+    {
+        public void DeleteArtifacts()
+        {
+            var pathToTempDirectory = Path.Combine(Path.GetTempPath(), "ETLArtifacts");
+
+            var files = Directory.EnumerateFiles(pathToTempDirectory);
+
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
